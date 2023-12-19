af1bcc4122e427dbc8b66eae6b48c5a505ea004d
MuhammadUsamaAwan <underusama@gmail.com>
2023-12-19 17:45:43 +0500

    setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,38 @@
+import { clsx, type ClassValue } from 'clsx';
+import { toast } from 'sonner';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function catchError(e: unknown) {
+  if (e instanceof Error) {
+    return toast.error(e.message);
+  } else {
+    return toast.error('Something went wrong, please try again later.');
+  }
+}
+
+export function formatDate(date: Date | string | number) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(new Date(date));
+}
+
+export function formatDateTime(date: Date | string | number) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(new Date(date));
+}
+
+export function truncate(str: string, length: number) {
+  return str.length > length ? `${str.substring(0, length)}...` : str;
+}
