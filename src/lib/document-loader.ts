c068490a65cee003161e39526baa771df2d28cb0
MuhammadUsamaAwan <underusama@gmail.com>
2023-12-22 14:51:47 +0500

    refactor

--- src/lib/document-loader.ts
+++ src/lib/document-loader.ts
@@ -1,65 +1,48 @@
 import { CSVLoader } from 'langchain/document_loaders/fs/csv';
 import { DocxLoader } from 'langchain/document_loaders/fs/docx';
-import { JSONLoader } from 'langchain/document_loaders/fs/json';
+import { EPubLoader } from 'langchain/document_loaders/fs/epub';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { PPTXLoader } from 'langchain/document_loaders/fs/pptx';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { UnstructuredLoader } from 'langchain/document_loaders/fs/unstructured';
 
-async function pdfLoader(path: string) {
-  const loader = new PDFLoader(path);
-  return loader.load();
-}
-
-async function csvLoader(path: string) {
-  const loader = new CSVLoader(path);
-  return loader.load();
-}
-
-async function jsonLoader(path: string) {
-  const loader = new JSONLoader(path);
-  return loader.load();
-}
-
-async function docxLoader(path: string) {
-  const loader = new DocxLoader(path);
-  return loader.load();
-}
-
-async function pptxLoader(path: string) {
-  const loader = new PPTXLoader(path);
-  return loader.load();
-}
-
-async function textLoader(path: string) {
-  const loader = new TextLoader(path);
-  return loader.load();
-}
-
-async function unstructuredLoader(path: string) {
-  const loader = new UnstructuredLoader(path, {
-    apiUrl: 'http://127.0.0.1:8001/general/v0/general',
-  });
-  return loader.load();
-}
-
-export async function loadDocument(path: string) {
-  const extension = path.split('.').pop();
-  if (!extension) throw new Error('Unable to determine file extension');
-  switch (extension) {
+function getDocumentLoader(filePath: string) {
+  const fileType = filePath.split('.').pop();
+  let loader;
+  switch (fileType) {
     case 'pdf':
-      return pdfLoader(path);
-    case 'csv':
-      return csvLoader(path);
-    case 'json':
-      return jsonLoader(path);
+      loader = new PDFLoader(filePath, {
+        splitPages: false,
+      });
+      return loader;
+    case 'epub':
+      loader = new EPubLoader(filePath, {
+        splitChapters: false,
+      });
+      return loader;
     case 'docx':
-      return docxLoader(path);
-    case 'pptx':
-      return pptxLoader(path);
+      loader = new DocxLoader(filePath);
+      return loader;
     case 'txt':
-      return textLoader(path);
+      loader = new TextLoader(filePath);
+      return loader;
+    case 'md':
+      loader = new TextLoader(filePath);
+      return loader;
+    case 'json':
+      loader = new TextLoader(filePath);
+      return loader;
+    case 'csv':
+      loader = new CSVLoader(filePath);
+      return loader;
     default:
-      return unstructuredLoader(path);
+      loader = new UnstructuredLoader(filePath, {
+        apiUrl: 'http://127.0.0.1:8001/general/v0/general',
+      });
+      return loader;
   }
+}
+
+export function loadDocument(filePath: string) {
+  const loader = getDocumentLoader(filePath);
+  return loader.loadAndSplit();
 }
